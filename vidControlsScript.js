559266df5675ab4006c215fa8a9ce20fd0ba901d
dawrentz <156712569+dawrentz@users.noreply.github.com>
2025-03-21 11:13:20 -0500

    Add README and instructions

--- vidControlsScript.js
+++ vidControlsScript.js
@@ -4,16 +4,30 @@
 // GitHub: https://github.com/joshuaGmartin
 // LinkedIn: https://www.linkedin.com/in/joshua-martin-697457277/
 //
-// Description: Adds keyboard shortcuts for enhanced video playback control
+// Description: Adds keyboard shortcuts for enhanced video playback control.
+//
+// Instructions:
+//    1. Open a Percipio video at https://wguacademy.percipio.com/videos/
+//    2. Press F12 to open the Developer Tools console
+//    3. Paste the script into the console and press Enter
+//    4. Press F12 again to close Developer Tools
+//    5. Use the hotkeys (listed below) for an enhanced video playback experience
+//
+// Notes:
+//    1. The script must be reloaded each time a Percipio video page is opened.
+//       a. After running the script once, you can press the Up Arrow in the console to quickly reuse the last command without pasting.
+//
+//    2. On rare occasions, the webpage may focus on the video player's Play button, which overrides the hotkeys.
+//       a. To fix this, click anywhere outside the video player to remove focus from the Play button. Reload the script if necessary.
 //
 // Hotkeys:
-// Left Arrow   - Rewind 5 seconds (adjustable)
-// Right Arrow  - Skip forward (adjustable)
-// Space Bar    - Play/Pause
-// "f"          - Toggle fullscreen
-// "c"          - Toggle captions
-// "<"          - Decrease playback speed (adjustable)
-// ">"          - Increase playback speed (adjustable)
+//    Left Arrow  - Rewind 5 seconds (adjustable)
+//    Right Arrow - Skip forward 5 seconds (adjustable)
+//    Space Bar   - Play/Pause
+//    "f"         - Toggle fullscreen
+//    "c"         - Toggle captions
+//    "<"         - Decrease playback speed (adjustable)
+//    ">"         - Increase playback speed (adjustable)
 //
 // ====================================================
 
